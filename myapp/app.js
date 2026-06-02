69abfad7e6fc91f81873d83b1cd0ac10b2f05585
shu <tyaro.rona@gmail.com>
2026-06-02 15:31:05 +0900

    ポート番号を環境変数から読むように変更

--- myapp/app.js
+++ myapp/app.js
@@ -1,28 +1,55 @@
+require('dotenv').config();
 const express = require('express');
+const { Pool } = require('pg');
+
 const app = express();
-const PORT = 3000;
 
-// JSONを扱うために必要
 app.use(express.json());
-
-// 静的ファイル
 app.use(express.static('public'));
 
-// POSTエンドポイント
-app.post('/api/messages', (req, res) => {
-  // 分割代入
-  const { username, message } = req.body;
-
-  // 省略記法
-  const newMessage = { username, message };
-
-  // ターミナルに表示
-  console.log(newMessage);
-
-  // レスポンス
-  res.json(newMessage);
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
 });
 
-app.listen(PORT, () => {
-  console.log(`サーバー起動 http://localhost:${PORT}`);
+// メッセージ一覧を取得
+app.get('/api/messages', async (req, res) => {
+  try {
+    const result = await pool.query(
+      'SELECT * FROM messages ORDER BY created_at ASC'
+    );
+
+    res.json(result.rows);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'DB取得エラー' });
+  }
+});
+
+// メッセージを保存
+app.post('/api/messages', async (req, res) => {
+  try {
+    const { username, message } = req.body;
+
+    const result = await pool.query(
+      'INSERT INTO messages (username, text) VALUES ($1, $2) RETURNING *',
+      [username, message]
+    );
+
+    res.json(result.rows[0]);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'DB保存エラー' });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(
+    `サーバが起動しました： http://localhost:${process.env.PORT || 3000}`,
+  );
 });
